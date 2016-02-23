e9ac8cbdbc45ecd62ae4d78a1fd0c776e035f8eb
Dionysios Barmpoutis <barmpoutis@gmail.com>
2016-02-23 06:16:48 -0500

    template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    int T; std::cin >> T;
+    for(int a = 1; a <= T; a++){
+
+
+        std::cout << "Case #" << a << ": " << res << std::endl;
+
+    }
+
+    return 0;
+}
